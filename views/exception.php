03c0ec529d5bee407cd7a873c418a356b6a29e6c
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2014-11-06 16:55:56 +0100

    forgot a view file

---
+++ views/exception.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <script src="<?PHP echo $shopUrl; ?>/public/scripts/mootools.js"></script>
+    <script src="https://developers.shoper.pl/public/sdk.js"></script>
+
+    <script>var app = new ShopApp(function(app) {
+            app.init(null, function(params, app) {
+                for(var x = 0; x < params.styles.length; ++x) {
+                    var el = document.createElement('link');
+                    el.rel = 'stylesheet';
+                    el.type = 'text/css';
+                    el.href = params.styles[x];
+                    document.getElementsByTagName('head')[0].appendChild(el);
+                }
+
+                app.show(null ,function () {
+                    app.adjustIframeSize();
+                });
+
+            }, function(errmsg, app) {
+                alert(errmsg);
+            });
+        }, true);
+
+
+    </script></head><body>
+
+<main class="rwd-layout-width rwd-layout-container">
+    <section class="rwd-layout-col-12">
+
+        <p><?PHP echo $message; ?></p>
+
+    </section></main>
+
+
+<body></html>
